de8f656b050926cf3d734e1099acb981c061f212
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-11-25 16:16:21 -0300

    types: adds type declaration for requests

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,49 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  type EMigrationStatus = "pending" | "processing" | "done";
+
+  type JdbcConnectionConfig = {
+    name: string;
+    jdbcUrl: string;
+    username: string;
+    password: string;
+    driverClassName: string;
+  };
+
+  type StartMigrationRequest = { name: string };
+
+  type StartExtractionRequest = {
+    id: string;
+    originConfig: JdbcConnectionConfig;
+  };
+
+  type StartTransformationRequest = {
+    id: string;
+    target: string;
+  };
+
+  type StartLoadRequest = {
+    id: string;
+    targetConfig: JdbcConnectionConfig;
+  };
+
+  type StartValidationRequest = StartExtractionRequest & StartLoadRequest;
+
+  type ScriptFileInfo = {
+    filename: string;
+    content: string;
+  };
+
+  type UpdateScriptRequest = ScriptFileInfo[];
+
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
 
 export {};
