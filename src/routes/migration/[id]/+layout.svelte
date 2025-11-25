4cc83db53779b346282bbf26e44d422eb6014836
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-11-25 10:29:10 -0300

    feat: adds migration page

---
+++ src/routes/migration/[id]/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { StatusBadge, TextButton } from "$lib/components";
+  import { ChevronLeftIcon } from "@lucide/svelte";
+  import type { LayoutProps } from "./$types";
+
+  let { children, data }: LayoutProps = $props();
+</script>
+
+<section class="base-section">
+  <div class="space-y-8 w-7/10">
+    <span class="inline-flex gap-9 w-full">
+      <TextButton onclick={() => goto("/")}>
+        <ChevronLeftIcon class="size-8" />
+        Início
+      </TextButton>
+      <h2 class="title">Migração Teste ({data.id})</h2>
+
+      <StatusBadge status="pending" class="ml-auto" />
+    </span>
+
+    {@render children()}
+  </div>
+</section>
---
+++ src/routes/migration/[id]/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import {
+    ChevronLeftIcon,
+    ArrowRightIcon,
+    FileCodeIcon,
+    ChevronRightIcon,
+  } from "@lucide/svelte";
+  import { Input, TextButton } from "$lib/components";
+  import type { PageProps } from "./$types";
+
+  let { data }: PageProps = $props();
+</script>
+
+<div class="flex justify-between items-center gap-8">
+  <div class="card space-y-3">
+    <h3 class="title font-normal">Base de dados origem</h3>
+    <Input class="h-12" label="SGBD" />
+    <Input class="h-12" label="Nome/identificador da base de dados" />
+    <Input class="h-12" label="Senha da base de dados" />
+    <Input class="h-12" label="Usuário da base de dados" />
+    <Input class="h-12" label="Porta da base de dados" />
+  </div>
+
+  <ArrowRightIcon class="stroke-primary size-64" />
+
+  <div class="card space-y-3">
+    <h3 class="title font-normal">Base de dados destino</h3>
+    <Input class="h-12" label="SGBD" />
+    <Input class="h-12" label="Nome/identificador da base de dados" />
+    <Input class="h-12" label="Senha da base de dados" />
+    <Input class="h-12" label="Usuário da base de dados" />
+    <Input class="h-12" label="Porta da base de dados" disabled />
+  </div>
+</div>
+
+<span class="flex justify-between items-baseline px-4">
+  <TextButton>
+    <FileCodeIcon class="size-5 mr-2" />
+    Editar DDL
+  </TextButton>
+
+  <TextButton>
+    Iniciar Migração
+    <ChevronRightIcon class="size-5" />
+  </TextButton>
+</span>
