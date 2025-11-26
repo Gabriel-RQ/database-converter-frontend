2a4954a0358ecdd168c3a559773af6aca200cba9
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-11-26 15:43:33 -0300

    update: footer

--- src/lib/components/structure/Footer.svelte
+++ src/lib/components/structure/Footer.svelte
@@ -1,3 +1,11 @@
-<footer class="text-center">
-  <p class="text-xs">Database Converter Tool v1 - 2025</p>
+<footer class="text-center text-xs">
+  <p>Database Converter Tool v1 - 2025</p>
+  <p>
+    Por <a
+      href="https://github.com/Gabriel-RQ"
+      target="_blank"
+      rel="noreferrer"
+      class="text-accent">Gabriel RQ</a
+    >
+  </p>
 </footer>
