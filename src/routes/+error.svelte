4c95fc972cd28c42a9d373cc33eb088dadf907b2
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-11-28 10:04:11 -0300

    feat: adds error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import { ChevronLeftIcon } from "@lucide/svelte";
+</script>
+
+<div class="size-full flex justify-center items-center">
+  <div class="space-y-6">
+    <h1 class="text-8xl font-bold">Oops...</h1>
+    <p class="text-xl">
+      {page.error?.message}
+    </p>
+    <a
+      href="/"
+      class="inline-flex gap-2 items-center text-xl text-primary hover:text-secondary focus:text-secondary"
+    >
+      <ChevronLeftIcon class="size-4" />
+      Melhor voltar
+    </a>
+  </div>
+</div>
