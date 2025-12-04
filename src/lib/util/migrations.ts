071bed6dc5f5a1c4612bb69b12898c1ddfc02486
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-12-04 16:49:08 -0300

    update: adds seconds to logs

--- src/lib/util/migrations.ts
+++ src/lib/util/migrations.ts
@@ -34,6 +34,7 @@ export function parseMigrationLog(status: MigrationStatusResponse) {
       year: "numeric",
       hour: "2-digit",
       minute: "2-digit",
+      second: "2-digit",
     }
   );
   return `${d} - ${getMigrationStepMessage(status.step, status.message)}`;
