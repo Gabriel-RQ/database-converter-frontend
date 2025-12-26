790480c16bc59703461b3fd996f9df35e0a813ce
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-12-26 16:53:16 -0300

    chore: adds jsdom and @testing-library/svelte packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,40 @@
-import { defineConfig } from 'vitest/config';
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from "vitest/config";
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit()],
 
-	test: {
-		expect: { requireAssertions: true },
+  test: {
+    expect: { requireAssertions: true },
+    projects: [
+      {
+        extends: "./vite.config.ts",
 
-		projects: [
-			{
-				extends: './vite.config.ts',
+        test: {
+          name: "server",
+          environment: "node",
+          include: ["src/**/*.{test,spec}.{js,ts}"],
+          exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+        },
+      },
 
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+      {
+        extends: "./vite.config.ts",
+
+        test: {
+          name: "client",
+
+          environment: "jsdom",
+          include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+          exclude: ["src/lib/server/**"],
+        },
+      },
+    ],
+  },
+  resolve: process.env.VITEST
+    ? {
+        conditions: ["browser"],
+      }
+    : undefined,
 });
